67aeb6e401ea1935b65102d85084b9e01033bc97
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-12-09 19:03:51 -0500

    notes on tuples aka lists that can't change

---
+++ 3_tuples_sets.py
@@ -0,0 +1,13 @@
+# A tuple is a collection which is ordered and unchangeable. 
+# Allows duplicate members. 
+fruits = ("Apples", "Oranges", "Grapes")
+# fruits2 = tuple(("Apples", "Oranges", "Grapes"))
+
+fruits2 = ('Apples',)
+# A comma is what makes a tuple a tuple
+
+# Get value
+print(fruits[1])
+
+print(fruits2,type(fruits2))
+# A set is a collection which is unordered and unindexed. No duplicate members. 
