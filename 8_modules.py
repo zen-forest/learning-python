b835c3fc1ef695ed78668b2bb14e7b8476f8b84a
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-12-14 00:57:34 -0500

    updated file names

---
+++ 8_modules.py
@@ -0,0 +1,4 @@
+# A module is basically a file containing 
+# a set of functions to include in your application
+# there are core python modules, modules you can install using the pip package manager
+# (including Django) as well as custom modules
